80a5f0c1c4caa0eb65d2bc720052b18c2ecd185c
MarioLaranjeiro <75047165+MarioLaranjeiro@users.noreply.github.com>
2024-05-05 00:24:37 +0100

    Update runGold.m

--- runGold.m
+++ runGold.m
@@ -1,47 +1,48 @@
 function [K, R, t, error] = runGold(xy, XYZ, Dec_type)
+% Função para executar a otimização da DLT usando busca áurea.
 
-%normalize data points
-[xy_normalized, XYZ_normalized, T, U] = normalization(xy, XYZ); % (Como no runDLT)
+% Normalizar os pontos de dados
+[xy_normalized, XYZ_normalized, T, U] = normalization(xy, XYZ);
 
-%compute DLT
+% Calcular a DLT
 [Pn] = dlt(xy_normalized, XYZ_normalized);
 
-%minimize geometric error
-pn = [Pn(1,:) Pn(2,:) Pn(3,:)];
-for i=1:20
+% Minimizar o erro geométrico
+pn = [Pn(1, :) Pn(2, :) Pn(3, :)];
+for i = 1:20
     [pn] = fminsearch(@fminGold, pn, [], xy_normalized, XYZ_normalized);
 end
 
-P = [pn(1:4);pn(5:8);pn(9:12)];
+P = [pn(1:4); pn(5:8); pn(9:12)];
 
-%denormalize camera matrix
+% Denormalizar a matriz da câmera
 M = inv(T) * P * U;
 
-%factorize camera matrix in to K, R and t (Como no runDLT)
+% Fatorar a matriz da câmera em K, R e t
 if Dec_type == "QR"
-   [K, R, C] = decomposeQR(M); 
+    [K, R, C] = decomposeQR(M);
 elseif Dec_type == "EXP"
-   [K, R, C] = decomposeEXP(M);
+    [K, R, C] = decomposeEXP(M);
 else
-    disp('Error');
+    disp('Erro: Tipo de decomposição inválido');
 end
-t=-R*C;
+t = -R * C; % Calcular o vetor de translação
 
-%compute reprojection error
-n = size(XYZ,2);
-xy_reprojection = zeros(2,n);
+% Calcular o erro de reprojeção
+n = size(XYZ, 2);
+xy_reprojection = zeros(2, n);
 
 for i = 1:n
-    xy_reprojection(1,i) = (M(1,:)* [XYZ(:,i);1])/(M(3,:) * [XYZ(:,i);1]);
-    xy_reprojection(2,i) = (M(2,:)* [XYZ(:,i);1])/(M(3,:) * [XYZ(:,i);1]);
+    xy_reprojection(1, i) = (M(1, :) * [XYZ(:, i); 1]) / (M(3, :) * [XYZ(:, i); 1]);
+    xy_reprojection(2, i) = (M(2, :) * [XYZ(:, i); 1]) / (M(3, :) * [XYZ(:, i); 1]);
 end
 
-IMG_NAME = 'images/image001.jpg';
-img_I = imread(IMG_NAME);
-imshow(img_I);
+% Plotar os pontos originais e os pontos reprojetados na imagem
+IMG_NAME = 'images/image001.jpg'; % Nome do arquivo de imagem
+img_I = imread(IMG_NAME); % Ler a imagem
+imshow(img_I); % Mostrar a imagem
 hold on;
-plot(xy(1,:), xy(2,:), 'rx', 'LineWidth', 1, 'MarkerSize', 10);
-plot(xy_reprojection(1,:), xy_reprojection(2,:), 'bo', 'Color', 'g', 'LineWidth', 2, 'MarkerSize', 10);
-error = sum(sqrt(sum((xy_reprojection-xy).^2,1)).^2)/ size(xy,2);
-
+plot(xy(1, :), xy(2, :), 'rx', 'LineWidth', 1, 'MarkerSize', 10); % Plotar os pontos originais
+plot(xy_reprojection(1, :), xy_reprojection(2, :), 'bo', 'Color', 'g', 'LineWidth', 2, 'MarkerSize', 10); % Plotar os pontos reprojetados
+error = sum(sqrt(sum((xy_reprojection - xy).^2, 1)).^2) / size(xy, 2); % Calcular o erro de reprojeção
 end
