06fbacf916cb657f0fa251f963938e093f1ec93f
MarioLaranjeiro <75047165+MarioLaranjeiro@users.noreply.github.com>
2024-05-05 00:21:16 +0100

    Update dlt.m

--- dlt.m
+++ dlt.m
@@ -1,18 +1,19 @@
 function [P] = dlt(xy, XYZ)
-%computes DLT, xy and XYZ should be normalized before calling this function
+% Função para calcular a DLT (Direct Linear Transformation).
+% Os pontos xy e XYZ devem ser normalizados antes de chamar esta função.
 
-% Construct matrix A
+% Construir a matriz A
 n = size(xy, 2);
-A = zeros(2*n, 12);
+A = zeros(2 * n, 12);
 w = 1;
 
 for i = 1:n
-    A(2*i-1,:) = [w*XYZ(:,i)' 0 0 0 0 -xy(1,i)*XYZ(:,i)'];
-    A(2*i,:) =[0 0 0 0 w*XYZ(:,i)' -xy(2,i)*XYZ(:,i)'];
+    % Preencher as linhas pares e ímpares de A
+    A(2 * i - 1, :) = [w * XYZ(:, i)', 0, 0, 0, 0, -xy(1, i) * XYZ(:, i)'];
+    A(2 * i, :) = [0, 0, 0, 0, w * XYZ(:, i)', -xy(2, i) * XYZ(:, i)'];
 end
 
-% Solve for P using SVD
+% Resolver para P usando SVD
 [~, ~, V] = svd(A);
-P = reshape(V(:,end), 4, 3)';
-
+P = reshape(V(:, end), 4, 3)'; % Reformular o vetor coluna em uma matriz 4x3
 end
