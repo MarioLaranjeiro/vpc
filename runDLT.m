b49754fb03291523ea1fc104898aebb2cbbc9a68
MarioLaranjeiro <75047165+MarioLaranjeiro@users.noreply.github.com>
2024-05-05 00:24:06 +0100

    Update runDLT.m

--- runDLT.m
+++ runDLT.m
@@ -1,40 +1,40 @@
 function [K, R, t, error] = runDLT(xy, XYZ, D_type)
+% Função para executar a técnica de DLT (Direct Linear Transformation).
 
-%normalize data points
+% Normalizar os pontos de dados
 [xy_normalized, XYZ_normalized, T, U] = normalization(xy, XYZ);
 
-%compute DLT
+% Calcular a DLT
 [P_normalized] = dlt(xy_normalized, XYZ_normalized);
 
-%denormalize camera matrix
-M = inv(T)*P_normalized * U;
+% Denormalizar a matriz da câmera
+M = inv(T) * P_normalized * U;
 
-%factorize camera matrix in to K, R and t
+% Fatorar a matriz da câmera em K, R e t
 if D_type == "QR"
-   [K, R, C] = decomposeQR(M); 
+    [K, R, C] = decomposeQR(M);
 elseif D_type == "EXP"
-   [K, R, C] = decomposeEXP(M);
+    [K, R, C] = decomposeEXP(M);
 else
-    disp('Error');
+    disp('Erro: Tipo de decomposição inválido');
 end
-t=-R*C;
+t = -R * C; % Calcular o vetor de translação
 
-
-%compute reprojection error
-n = size(XYZ,2);
-xy_reprojection = zeros(2,n);
+% Calcular o erro de reprojeção
+n = size(XYZ, 2);
+xy_reprojection = zeros(2, n);
 
 for i = 1:n
-    xy_reprojection(1,i) = (M(1,:)* [XYZ(:,i);1])/(M(3,:) * [XYZ(:,i);1]);
-    xy_reprojection(2,i) = (M(2,:)* [XYZ(:,i);1])/(M(3,:) * [XYZ(:,i);1]);
+    xy_reprojection(1, i) = (M(1, :) * [XYZ(:, i); 1]) / (M(3, :) * [XYZ(:, i); 1]);
+    xy_reprojection(2, i) = (M(2, :) * [XYZ(:, i); 1]) / (M(3, :) * [XYZ(:, i); 1]);
 end
 
-IMG_NAME = 'images/image001.jpg';
-img_I = imread(IMG_NAME);
-imshow(img_I);
+% Plotar os pontos originais e os pontos reprojetados na imagem
+IMG_NAME = 'images/image001.jpg'; % Nome do arquivo de imagem
+img_I = imread(IMG_NAME); % Ler a imagem
+imshow(img_I); % Mostrar a imagem
 hold on;
-plot(xy(1,:), xy(2,:), 'rx', 'LineWidth', 1, 'MarkerSize', 10);
-plot(xy_reprojection(1,:), xy_reprojection(2,:), 'bo', 'LineWidth', 2, 'MarkerSize', 10);
-error = sum(sqrt(sum((xy_reprojection-xy).^2,1)).^2)/ size(xy,2);
-
+plot(xy(1, :), xy(2, :), 'rx', 'LineWidth', 1, 'MarkerSize', 10); % Plotar os pontos originais
+plot(xy_reprojection(1, :), xy_reprojection(2, :), 'bo', 'LineWidth', 2, 'MarkerSize', 10); % Plotar os pontos reprojetados
+error = sum(sqrt(sum((xy_reprojection - xy).^2, 1)).^2) / size(xy, 2); % Calcular o erro de reprojeção
 end
