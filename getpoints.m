a34ce971e0bee11a5756615b0fb4ca1faf355729
MarioLaranjeiro <75047165+MarioLaranjeiro@users.noreply.github.com>
2024-05-05 00:23:02 +0100

    Update getpoints.m

--- getpoints.m
+++ getpoints.m
@@ -1,24 +1,25 @@
 function [xy, XYZ] = getpoints(img)
+% Função para obter pontos em uma imagem e suas coordenadas 3D correspondentes.
 
-% Initially, the list of points is empty.
+% Inicialmente, a lista de pontos está vazia.
 xy = [];
 XYZ = [];
 n = 0;
-% Loop, picking up the points.
-disp('Left mouse button picks points.')
-disp('Right mouse button picks last point.')
-but = 1;
+
+% Loop para selecionar os pontos.
+disp('Pressione o botão esquerdo do mouse para selecionar pontos.')
+disp('Pressione o botão direito do mouse para finalizar a seleção.')
 zoom on;
+but = 1;
 while but == 1
-    %keyboard;
-    [xi, yi, but] = ginput(1);
+    [xi, yi, but] = ginput(1); % Captura as coordenadas (x, y) do clique do mouse
     hold on;
-    plot(xi, yi, 'ro')
+    plot(xi, yi, 'ro') % Plota um ponto vermelho na posição selecionada
     hold off;
-    n = n+1;
-    xy(:, n) = [xi; yi]; % add a new column with the current values
-    input = inputdlg('[X Y Z]'); % show input dialog
-    XYZi = str2num(input{1}); % convert to number
-    XYZ(:, n) = XYZi; % add a new column with the current values
+    n = n + 1;
+    xy(:, n) = [xi; yi]; % Adiciona um novo ponto à lista de pontos 2D
+    input = inputdlg('[X Y Z]'); % Mostra uma caixa de diálogo para inserir as coordenadas 3D
+    XYZi = str2num(input{1}); % Converte as coordenadas 3D de string para números
+    XYZ(:, n) = XYZi; % Adiciona um novo ponto à lista de pontos 3D
 end
 end
