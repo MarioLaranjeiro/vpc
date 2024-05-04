0dd98fd78138ed9684d17f60b1b5c5081692bf45
MarioLaranjeiro <75047165+MarioLaranjeiro@users.noreply.github.com>
2024-05-05 00:20:51 +0100

    Update decomposeQR.m

--- decomposeQR.m
+++ decomposeQR.m
@@ -1,24 +1,24 @@
 function [ K, R, C ] = decomposeQR(P)
-%decompose P into K, R and t
+% Função para decompor a matriz de projeção P em K (matriz de calibração),
+% R (matriz de rotação) e C (vetor de translação).
 
-% Extract the left 3x3 submatrix of P
-S = P(:,1:3);
+% Extrair a submatriz esquerda 3x3 de P
+S = P(:, 1:3);
 inv_S = inv(S);
 
-% Compute the QR decomposition of S^(-1)
+% Calcular a decomposição QR de S^(-1)
 [Q_qr, R_qr] = qr(inv_S);
 
-% Compute K and R
+% Calcular K e R
 K = inv(R_qr);
-K = K./K(end,end);
+K = K ./ K(end, end); % Normalizar K
 R = inv(Q_qr);
 
-% Compute the camera center C
-[~,~,V] = svd(P);
-C_matrix = V(:,end);
+% Calcular o centro da câmera C
+[~, ~, V] = svd(P);
+C_matrix = V(:, end);
 
-C = [C_matrix(1)/C_matrix(4);
-     C_matrix(2)/C_matrix(4);
-     C_matrix(3)/C_matrix(4)];
-
-end
+% Normalizar C_matrix pelo último elemento
+C = [C_matrix(1) / C_matrix(4);
+     C_matrix(2) / C_matrix(4);
+     C_matrix(3) / C_matrix(4)];
