51a2a3aae724010002f3d3d8d5fe6cebf63e44e9
MarioLaranjeiro <75047165+MarioLaranjeiro@users.noreply.github.com>
2024-05-05 00:23:35 +0100

    Update normalization.m

--- normalization.m
+++ normalization.m
@@ -1,35 +1,36 @@
 function [xyn, XYZn, T, U] = normalization(xy, XYZ)
+% Função para normalizar os pontos 2D e 3D.
 
-%data normalization
-% homogenize the points
-xy(3,:) = 1;
-XYZ(4,:) = 1;
+% Normalização dos dados
+% Tornar os pontos homogêneos
+xy(3, :) = 1;
+XYZ(4, :) = 1;
 
-% compute centroid
+% Calcular o centróide
 xy_centroid = mean(xy, 2);
 XYZ_centroid = mean(XYZ, 2);
 
-% compute scale
+% Calcular a escala
 xyn = xy - xy_centroid;
 XYZn = XYZ - XYZ_centroid;
-xy_distances = sqrt(xyn(1,:).^2 + xyn(2,:).^2);
-XYZ_distances = sqrt(XYZn(1,:).^2 + XYZn(2,:).^2 + XYZn(3,:).^2);
+xy_distances = sqrt(xyn(1, :).^2 + xyn(2, :).^2);
+XYZ_distances = sqrt(XYZn(1, :).^2 + XYZn(2, :).^2 + XYZn(3, :).^2);
 xy_scale = mean(xy_distances) / sqrt(2);
 XYZ_scale = mean(XYZ_distances) / sqrt(3);
 
-% create T and U transformation matrices
-T = inv([xy_scale 0 xy_centroid(1);
-         0 xy_scale xy_centroid(2);
-         0 0 1]);
-U = inv([XYZ_scale 0 0 XYZ_centroid(1);
-         0 XYZ_scale 0 XYZ_centroid(2);
-         0 0 XYZ_scale XYZ_centroid(3);
-         0 0 0 1]);
+% Criar as matrizes de transformação T e U
+T = inv([xy_scale, 0, xy_centroid(1);
+         0, xy_scale, xy_centroid(2);
+         0, 0, 1]);
+U = inv([XYZ_scale, 0, 0, XYZ_centroid(1);
+         0, XYZ_scale, 0, XYZ_centroid(2);
+         0, 0, XYZ_scale, XYZ_centroid(3);
+         0, 0, 0, 1]);
 
-% normalize the points according to the transformations
+% Normalizar os pontos de acordo com as transformações
 xyn = T * xy;
 XYZn = U * XYZ;
-xyn(3,:) = 1;
-XYZn(4,:) = 1;
+xyn(3, :) = 1;
+XYZn(4, :) = 1;
 
 end
